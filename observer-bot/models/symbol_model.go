16a6549e693858fd2298173c8a79f3155c8c5c77
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-08-04 03:28:36 +0300

    Update symbol model is completed

--- observer-bot/models/symbol_model.go
+++ observer-bot/models/symbol_model.go
@@ -1,10 +1,40 @@
 package models
 
 import (
+	"encoding/json"
+
+	"github.com/adshao/go-binance/v2/futures"
+	"github.com/kaankoken/binance-quick-go/helper"
 	"github.com/oklog/ulid/v2"
 )
 
-type SymbolModel struct {
+type SymbolDtoModel struct {
 	Id     ulid.ULID
 	Symbol string
+}
+type SymbolModel struct {
+	Symbol string
+}
+
+func FilterSymbols(data *futures.ExchangeInfo, quoteAsset string) *[]SymbolDtoModel {
+	symbols := make([]SymbolDtoModel, 0)
+
+	for _, symbol := range data.Symbols {
+		if symbol.QuoteAsset == quoteAsset {
+			symbols = append(symbols, SymbolDtoModel{Id: ulid.Make(), Symbol: symbol.Symbol})
+		}
+	}
+
+	return &symbols
+}
+
+func ToSymbolModel(data interface{}) *[]SymbolModel {
+	model := []SymbolModel{}
+	body, err := json.Marshal(data)
+	helper.CheckError(err)
+
+	err = json.Unmarshal(body, &model)
+	helper.CheckError(err)
+
+	return &model
 }
